24aa3c621b72607d20fc503ca4476b4846653db8
Flavien DELANGLE <flaviendelangle@gmail.com>
2020-04-23 15:14:40 +0200

    APP-10763 Use React.forwardRef on cell components

--- src/cell/BooleanCell/BooleanCell.tsx
+++ src/cell/BooleanCell/BooleanCell.tsx
@@ -4,15 +4,19 @@ import { palette } from '@habx/ui-core'
 
 import IconCell, { IconCellProps } from '../IconCell'
 
-const BooleanCell: React.FunctionComponent<BooleanCellProps> = ({
-  value,
-  ...props
-}) => (
-  <IconCell
-    icon={value ? 'check' : 'close'}
-    color={value ? palette.green[600] : palette.orange[400]}
-    {...props}
-  />
+const BooleanCell = React.forwardRef<HTMLDivElement, BooleanCellProps>(
+  (props, ref) => {
+    const { value, ...rest } = props
+
+    return (
+      <IconCell
+        ref={ref}
+        icon={value ? 'check' : 'close'}
+        color={value ? palette.green[600] : palette.orange[400]}
+        {...rest}
+      />
+    )
+  }
 )
 
 interface BooleanCellProps extends Omit<IconCellProps, 'icon' | 'color'> {
