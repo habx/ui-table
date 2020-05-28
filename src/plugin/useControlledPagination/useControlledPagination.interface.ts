ab4f47edfd65af560372eb36b1530404b271d0b5
Jean Dessane <jean@habx.fr>
2020-05-28 09:33:24 +0200

    APP-11487: pageSizeOptions

--- src/plugin/useControlledPagination/useControlledPagination.interface.ts
+++ src/plugin/useControlledPagination/useControlledPagination.interface.ts
@@ -6,6 +6,7 @@ export type Pagination = {
 export interface UseControlledPaginationOptions<D extends {}> {
   onPaginationChange?: (pagination: Pagination) => void
   pagination?: Pagination
+  pageSizeOptions?: number[]
 }
 
 export interface UseControlledPaginationInstanceProps<D extends {}>
