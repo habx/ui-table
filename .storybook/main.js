1b4a02262b4ac6bb2089173412490d642637e698
Jean Dessane <jean@habx.fr>
2020-08-12 09:56:13 +0200

    fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,21 +3,6 @@ const path = require("path");
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   addons: [
-    {
-      name: '@storybook/preset-typescript',
-      options: {
-        transpileManager: true,
-        tsLoaderOptions: {
-          configFile: path.resolve(__dirname, '../tsconfig.json'),
-        },
-        tsDocgenLoaderOptions: {
-          tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-        },
-        include: [
-          path.resolve(__dirname, "../src"),
-        ]
-      },
-    },
     '@storybook/addon-knobs/register',
     '@storybook/addon-viewport/register',
     '@storybook/addon-actions/register'
