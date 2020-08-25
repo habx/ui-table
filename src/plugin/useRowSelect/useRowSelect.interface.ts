3b893ec9e0ac2a1655fe760257afeefe7f63b12a
Jean Dessane <jean9696@users.noreply.github.com>
2020-08-25 09:23:56 +0200

    Remove double interface

--- src/plugin/useRowSelect/useRowSelect.interface.ts
+++ src/plugin/useRowSelect/useRowSelect.interface.ts
@@ -12,11 +12,5 @@ export interface UseRowSelectOptions<D extends {}>
   canSelectAll?: boolean
 }
 
-export interface UseRowSelectOptions<D extends {}>
-  extends ReactTable.UseRowSelectOptions<D>,
-    UseRowSelectCellProps<D> {
-  canSelectAll?: boolean
-}
-
 export interface UseRowSelectInterfaceProps<D extends {}>
   extends UseRowSelectOptions<D> {}
