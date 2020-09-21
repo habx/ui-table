7d78e16cdcc4bb20bc0bee3d19026427b986a75d
Flavien DELANGLE <flaviendelangle@gmail.com>
2020-09-21 11:13:27 +0200

    APP-14269 Adapt BooleanFilter for controlled tables #release

--- src/filterMethod/booleanFilter.ts
+++ src/filterMethod/booleanFilter.ts
@@ -1,14 +1,8 @@
 import { get, has } from 'lodash'
 import { FilterType } from 'react-table'
 
-export enum BooleanFilterValues {
-  All = 0,
-  Truthy = 1,
-  Falsy = 2,
-}
-
 const booleanFilter: FilterType<any> = (rows, id, filterValue) => {
-  if (filterValue === BooleanFilterValues.All) {
+  if (filterValue == null) {
     return rows
   }
 
@@ -16,10 +10,7 @@ const booleanFilter: FilterType<any> = (rows, id, filterValue) => {
     const rawValue = get(row.values, id)
     const value = has(rawValue, 'value') ? !!rawValue.value : !!rawValue
 
-    return (
-      (value && filterValue === BooleanFilterValues.Truthy) ||
-      (!value && filterValue === BooleanFilterValues.Falsy)
-    )
+    return (value && filterValue === true) || (!value && filterValue === false)
   })
 }
 
